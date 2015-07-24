d7147b5bd9f5bfa7dc304b1ea763f93348bb1857
karande <vishalmkarande@gmail.com>
2015-07-23 17:04:30 -0700

    added linear search algorithm

--- src/Search.java
+++ src/Search.java
@@ -2,5 +2,13 @@
  * Created by vkarande on 7/23/15.
  */
 public class Search {
+    public int linearSearch(int[] array, int key) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == key) {
+                return i;
+            }
+        }
 
+        return -1;
+    }
 }
